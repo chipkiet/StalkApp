7359dc72376dc2a313c703e1cfc870a2dc4ee4e5
ThienNLH3 <thiennlh3@fpt.com>
2026-07-28 23:08:19 +0700

    Fix UI render missing ImageCard block in CanvasBoard

---
+++ update_image_card.py
@@ -0,0 +1,13 @@
+﻿import re
+
+image_card_file = r'ChatApp.Client\Components\Pinboard\ImageCard.razor'
+with open(image_card_file, 'r', encoding='utf-8') as f:
+    content = f.read()
+
+content = content.replace("OnConnectRequested", "OnConnectClicked")
+content = content.replace("OnDeleteRequested", "OnTaskDeleted")
+
+with open(image_card_file, 'w', encoding='utf-8') as f:
+    f.write(content)
+
+print("ImageCard.razor updated")
